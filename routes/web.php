ce3bb62b908cf95469be09d8650bda35fcb4d874
Arjaball <johnarjaydacuyan@gmail.com>
2026-06-08 16:29:37 +0800

    Render deploy route

--- routes/web.php
+++ routes/web.php
@@ -8,53 +8,43 @@ use Inertia\Inertia;
 | Web Routes
 |--------------------------------------------------------------------------
 */
-// Default route (optional redirect)
+
 Route::get('/', function () {
-    return redirect('Main/dashboard');
+    return redirect('/dashboard');
 });
 
 // * Main Pages
-
-// Dashboard
 Route::get('/dashboard', function () {
     return Inertia::render('Main/Dashboard');
 });
 
-//  Requests Trip Ticket
+// * Requester Pages
 Route::get('/request-trip-ticket', function () {
     return Inertia::render('Requester/RequestTripTickets');
 });
 
-// My Tickets
 Route::get('/my-tickets', function () {
     return Inertia::render('Requester/MyTickets');
 });
 
 // * Processor Pages
-
-// Incoming Queue
 Route::get('/incoming-queue', function () {
     return Inertia::render('Processor/IncomingQueue');
 });
 
-// Assign & Review
 Route::get('/assign-review', function () {
     return Inertia::render('Processor/AssignReview');
 });
 
 // * Data Management Pages
-
-// Drivers
 Route::get('/drivers', function () {
     return Inertia::render('DataManagement/Drivers');
 });
 
-// Users
 Route::get('/users', function () {
     return Inertia::render('DataManagement/Users');
 });
 
-// Vehicles
 Route::get('/vehicles', function () {
     return Inertia::render('DataManagement/Vehicles');
 });
