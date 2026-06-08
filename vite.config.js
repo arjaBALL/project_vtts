3f94f67f697939c946bafbc7b99a7dc5c8372104
Arjaball <johnarjaydacuyan@gmail.com>
2026-06-08 16:50:47 +0800

    5 Docker & render deployment

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [],
+            input: ["resources/css/app.css", "resources/js/app.jsx"],
             refresh: true,
         }),
         react(),
